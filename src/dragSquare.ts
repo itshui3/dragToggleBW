8b985d85c46bff94e11d7bdc919e48cec7926d75
Hui <xuhuizhu369@gmail.com>
2021-03-24 17:08:17 -0700

    targets mouseup'ed corners and affects them

--- src/dragSquare.ts
+++ src/dragSquare.ts
@@ -18,13 +18,35 @@ export const buildDragSquare = () => {
         const handleMove = (moveEV: MouseEvent) => {
             Esquare.style.left = (moveEV.clientX - offsetX) + 'px';
             Esquare.style.top = (moveEV.clientY - offsetY) + 'px';
+
+            // Esquare.hidden = true;
+            // let elemAtPoint = document.elementFromPoint(moveEV.clientX, moveEV.clientY);
+            // Esquare.hidden = false;
+
+            // if (!elemAtPoint) {return}
+            // if (elemAtPoint.className !== 'cornerSquare') {return}
+
+            // console.log(elemAtPoint);
+
         };
 
         document.addEventListener('mousemove', handleMove);
 
-        document.onmouseup = () => {
+        document.onmouseup = (muEV) => {
+            Esquare.hidden = true;
+            let elemAtPoint = document.elementFromPoint(muEV.clientX, muEV.clientY);
+            Esquare.hidden = false;
+
+            if (!elemAtPoint) {return}
+            if (elemAtPoint.className === 'cornerSquare') {
+                const droppedSquare = elemAtPoint as HTMLDivElement;
+                droppedSquare.style.backgroundColor = 'white';
+            }
+
+
             document.removeEventListener('mousemove', handleMove);
             document.onmouseup = null;
+
         };
 
     });
